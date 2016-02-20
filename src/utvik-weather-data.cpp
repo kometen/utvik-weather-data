3b0a9d89557b7dc3760af56c4a25f50ba4e1088d
Claus Guttesen <kometen@gmail.com>
2016-02-20 17:19:16 +0100

    Parse xml and insert into db.

--- src/utvik-weather-data.cpp
+++ src/utvik-weather-data.cpp
@@ -15,6 +15,8 @@
 #include <curlpp/Options.hpp>
 #include <curlpp/Exception.hpp>
 
+#include <pqxx/pqxx>
+
 #include "ParseXML.hpp"
 
 using namespace std;
@@ -25,35 +27,49 @@ int main(int argc, char* argv[]) {
         return EXIT_FAILURE;
     }
     try {
-        const string url = "https://www.vegvesen.no/ws/no/vegvesen/veg/trafikkpublikasjon/vaer/1/GetMeasuredWeatherData/";
-        std::string username = argv[1];
-        std::string password = argv[2];
-        std::string credentials = username + ":" + password;
+        try {
+            pqxx::connection C("dbname=weather user=claus hostaddr=127.0.0.1 port=5432");
+            if (!C.is_open()) {
+                cerr << "Unable to connect to database" << C.dbname() << endl;
+                return EXIT_FAILURE;
+            }
 
-        curlpp::Cleanup cleaner;
-        curlpp::Easy request;
+            const string url = "https://www.vegvesen.no/ws/no/vegvesen/veg/trafikkpublikasjon/vaer/1/GetMeasuredWeatherData/";
 
-        request.setOpt(new curlpp::options::Url(url));
-        request.setOpt(new curlpp::options::UserPwd(credentials));
+            std::string username = argv[1];
+            std::string password = argv[2];
+            std::string credentials = username + ":" + password;
 
-        ostringstream out;
-        out << request;
+            curlpp::Cleanup cleaner;
+            curlpp::Easy request;
 
-        ParseXML parse{out.str()};
+            request.setOpt(new curlpp::options::Url(url));
+            request.setOpt(new curlpp::options::UserPwd(credentials));
 
-        cout << "Id: " << parse.id() << endl;
-        cout << "Date: " << parse.date() << endl;
-        cout << "Relative humidity: " << parse.relative_humidity() << " %" << endl;
-        cout << "Precipitation: " << parse.precipitation() << " mm" << endl;
-        cout << "Road surface temperature: " << parse.road_surface_temperature() << " ºC, " << parse.road_surface_temperature_fahrenheit() << " ºF" << endl;
-        cout << "Snow depth: " << parse.snowdepth() << " meters" << endl;
-        cout << "Windspeed: " << parse.windspeed() << " km/h, " << parse.windspeedMPS() << " m/s" << endl;
-        cout << "Winddirection: " << parse.winddirection() << endl;
-        cout << "Temperature: " << parse.temperature() << " ºC, " << parse.temperature_fahrenheit() << " ºF" << endl;
-        cout << "Dew point temperature: " << parse.dew_point_temperature() << " ºC, " << parse.dew_point_temperature_fahrenheit() << " ºF" << endl;
-        cout << "Visibility: " << parse.visibility() << " meters" << endl;
+            ostringstream out;
+            out << request;
 
-        return EXIT_SUCCESS;
+            ParseXML parse{out.str()};
+
+            string sql = "insert into readings (_id, date, relative_humidity, precipitation, road_surface_temperature";
+            sql += ", snow_depth, wind_speed, wind_direction, temperature, dew_point_temperature, visibility)";
+            sql += " values (" + parse.id() + ", '" + parse.date() + "'";
+            sql += ", " + to_string(parse.relative_humidity()) + ", " + to_string(parse.precipitation());
+            sql += ", " + to_string(parse.road_surface_temperature()) + ", " + to_string(parse.snowdepth());
+            sql += ", " + to_string(parse.windspeed()) + ", " + to_string(parse.winddirection());
+            sql += ", " + to_string(parse.temperature()) + ", " + to_string(parse.dew_point_temperature());
+            sql += ", " + to_string(parse.visibility()) + ")";
+
+            pqxx::work W(C);
+            W.exec(sql);
+            W.commit();
+            C.disconnect();
+            return EXIT_SUCCESS;
+        } catch (const exception& e) {
+            cerr << e.what() << endl;
+            return EXIT_FAILURE;
+        }
+
     } catch (curlpp::LogicError& e) {
         cout << e.what() << endl;
     } catch (curlpp::RuntimeError& e) {
